ce6344b947ae1339749a8b8b9f942c530ab4dd20
Scott McCoy <scotthmccoy@gmail.com>
2015-02-20 23:55:56 -0800

    WIP: Having some issues getting the scrollview to behave with Autolayout

---
+++ TapletCodingChallenge/ExtractedImageScrollViewController.m
@@ -0,0 +1,13 @@
+//
+//  ExtractedImageScrollViewController.m
+//  TapletCodingChallenge
+//
+//  Created by Scott McCoy on 2/20/15.
+//  Copyright (c) 2015 ScottSoft. All rights reserved.
+//
+
+#import "ExtractedImageScrollViewController.h"
+
+@implementation ExtractedImageScrollViewController
+
+@end
